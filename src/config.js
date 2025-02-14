912e666e31f35a164dc2a5a538d497f99bd4b8c0
MJ933 <mojtabaaa006@gmail.com>
2025-02-14 15:37:36 +0300

    fix the contact numbers

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 export const socialMediaLinks = {
   facebook: "/",
   instagram: "/",
-  telegram: "https://t.me/+9647810391757", // Replace with your actual username or +964... for the phone number
-  whatsapp: "https://wa.me/9647810391757", // International format
+  telegram: "https://t.me/+9647816176947", // Replace with your actual username or +964... for the phone number
+  whatsapp: "https://wa.me/9647816176947", // International format
   twitter: "/",
 };
